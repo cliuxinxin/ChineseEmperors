1ccd33fdbee79948f5d4605975ca18c31b36b238
cliuxinxin <cliuxinxin@gmail.com>
2025-10-29 21:54:04 +0800

    修复路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ChineseEmperors/',
+  base: process.env.NODE_ENV === 'production' ? '/ChineseEmperors/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true
